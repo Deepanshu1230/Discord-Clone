7059dd479e392914029b2f2650020395b40d1dd6
Deepanshu1230 <144600350+Deepanshu1230@users.noreply.github.com>
2024-09-08 19:01:26 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  fontFamily:{
+    Whitney:["Whitney"],
+    Ginto:["Ginto"],
+    ggsans:["ggsans"],
+    GintoNord:["GintoNord"],
+  },
+}
